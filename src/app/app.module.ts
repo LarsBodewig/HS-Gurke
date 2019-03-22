ebf52f79c2b8d4ca05f0e60b36a56a4b3d386336
fan2404 <fan2404@aol.com>
2019-03-22 18:17:04 +0100

    Split login page in frame and components Added custom CSS variables Added favicon and logo to assets Centered component in frame Placed logo over component in frame Added grid layout to login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,11 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/login-module/page/page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.scss'],
+})
+export class PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
