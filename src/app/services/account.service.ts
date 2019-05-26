a981ed7e72044b51c21188502e69262dc7624d30
fan2404 <fan2404@aol.com>
2019-05-26 18:25:53 +0200

    Implemented redirect to requested page after login Added account interface Implemented LoginGuard to redirect on permitted navigation Added TopicGuard

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Account } from '../models/account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
+  private account: Account;
+  private redirect: string;
+
   constructor() { }
 
-  public login(data: { email: string, password: string }): Promise<boolean> {
-
-    return Promise.resolve(false);
+  public login(data: { email: string, password: string }): Promise<{ granted: boolean, redirect?: string, reason?: string }> {
+    this.account = {};
+    return Promise.resolve({ granted: true, redirect: this.getRedirect() });
   }
 
   public register(data: { email: string, password: string, passwordVerify: string }): Promise<{ granted: boolean, reason?: string }> {
@@ -22,8 +26,24 @@ export class AccountService {
     return Promise.resolve({ granted: false, reason: 'Der eingegebene Sicherheitscode stimmt nicht mit dem hinterlegten überein.' });
   }
 
-  public terminate(): Promise<boolean> {
+  public terminate(data: { email: string, code: string }): Promise<{ granted: boolean, reason?: string }> {
 
-    return Promise.resolve(true);
+    return Promise.resolve({ granted: true });
+  }
+
+  public isLoggedIn(): boolean {
+    return this.account != undefined;
+  }
+
+  setRedirect(url: string): void {
+    this.redirect = url;
+  }
+
+  clearRedirect(): void {
+    this.redirect = undefined;
+  }
+
+  getRedirect(): string {
+    return this.redirect;
   }
 }
