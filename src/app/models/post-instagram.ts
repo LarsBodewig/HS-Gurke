2e8574e95cdd718da29041aa62bb51d819dac719
Jakob Engelhardt <userone@Jakobs-Air.Speedport_W723_V_Typ_A_1_01_018>
2019-06-11 21:26:09 +0200

    Creating interface for twitter and instagram postings

---
+++ src/app/models/post-instagram.ts
@@ -0,0 +1,27 @@
+export interface Post {
+  author: string;
+  avi: string;
+  post: string;
+  caption: string;
+  timestamp: date;
+  likes: number;
+  comments: number;
+  source: string;
+}
+}
+  /*equals(post: Post): boolean {
+
+    return this.author === post.author
+      && this.avi === post.avi
+      && this.post === post.post
+      && this.caption === post.caption
+      && this.timestamp === post.timestamp
+      && this.likes === post.likes
+      && this.comments === post.comments
+      && this.source === post.source;
+  }*/
+
+  public get() {
+
+  }
+}
