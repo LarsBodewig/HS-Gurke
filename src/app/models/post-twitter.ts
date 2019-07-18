6df9feb1a2118a782b98e2aaf669a08451ee16bf
fan2404 <fan2404@aol.com>
2019-07-18 18:16:53 +0200

    Added JSON posts from twitter Updated TwitterPost interface Added DataService to read JSON data Created grid card layout in TopicComponent for tweets Added color highlighting for active MenuItem in SideMenuComponent

--- src/app/models/post-twitter.ts
+++ src/app/models/post-twitter.ts
@@ -1,22 +1,11 @@
-export interface Post {
-  author: string;
-  avi: string;
-  post: string;
-  timestamp: Date;
-  likes: number;
-  photo: string;
-  comments: number;
-  source: string;
+export interface TwitterPost {
+  id: String;
+  author: String;
+  source: String;
+  post: String;
+  timestamp: number;
+  reply: String;
+  retweet: boolean;
+  avatar: String;
+  media: String;
 }
-
-  /*equals(post: Post): boolean {
-
-    return this.author === post.author
-      && this.avi === post.avi
-      && this.post === post.post
-      && this.timestamp === post.timestamp
-      && this.likes === post.likes
-      && this.photo === post.photo
-      && this.comments === post.comments
-      && this.source === post.source;
-  }*/
