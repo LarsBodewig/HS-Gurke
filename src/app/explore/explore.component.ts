919bf7c71a6bb995ad9e22842bb1c24f99a12e37
Jakob Engelhardt <userone@Jakobs-Air.Speedport_W723_V_Typ_A_1_01_018>
2019-07-20 20:14:16 +0200

    Displayed posts in a responsive grid layout

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,14 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+function resizeGridItem(item){
+  let grid:Element = document.getElementsByClassName("grid")[0];
+  var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  console.log(item.outerHTML);
+  var rowSpan = Math.ceil((item.getElementsByClassName("post")[0].getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.style.gridRowEnd = "span "+rowSpan;
+}
+
+function resizeAllGridItems() {
+  var allItems = document.getElementsByClassName("post-card");
+  for(var x=0;x<allItems.length;x++){
+    resizeGridItem(allItems[x]);
+  }
+}
+
+function resizeInstance(instance){
+    var item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+//window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+var allItems = document.getElementsByClassName("post-card");
+for(var x=0;x<allItems.length;x++){
+//  imagesLoaded( allItems[x], resizeInstance);
+}
+
+
 @Component({
   selector: 'explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss'],
 })
+
 export class ExploreComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() { }
+  ngAfterViewInit() {
+    setTimeout(()=>resizeAllGridItems(),100);
+  }
+  ngOnInit () {
 
+  }
 }
